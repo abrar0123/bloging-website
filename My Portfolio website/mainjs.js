34ff3cd884287c23d6a51c86d10aad3452a5ccee
abrar0123 <104258004+abrar0123@users.noreply.github.com>
2022-11-21 11:21:20 +0500

    Add files via upload

---
+++ My Portfolio website/mainjs.js
@@ -0,0 +1,10 @@
+var navLinks=document.getElementById("navLinks");
+function showMenu(){
+    navLinks.style.right="0";
+    console.log("my show menu ")
+
+}
+function hideMenu(){
+    navLinks.style.right="-200px";
+    
+}
